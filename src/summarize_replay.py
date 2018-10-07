22f8cafddc22df13f5a8f8f34d5e87f0b7f7f2d1
Eric Denovellis <edeno@bu.edu>
2018-10-07 10:01:28 -0400

    Consolidate overlap analysis to summarize replay

--- src/summarize_replay.py
+++ src/summarize_replay.py
@@ -111,7 +111,7 @@ def summarize_replays(replay_info, detector_results, decoder_results, data,
                           .rename('detector_posterior'))
     detector_likelihood = (xr.concat(
         detector_likelihood, dim=replay_info.index)
-                           .rename('detector_likelihood'))
+        .rename('detector_likelihood'))
     decoder_posterior = (xr.concat(decoder_posterior, dim=replay_info.index)
                          .rename('decoder_posterior'))
     replay_densities = xr.merge(
@@ -190,3 +190,62 @@ def _get_replay_movement(posterior_density):
 def _average_credible_interval_size(posterior_density):
     credible_interval = np.percentile(posterior_density, [2.5, 97.5], axis=1)
     return np.mean(np.diff(credible_interval, axis=0))
+
+
+def _get_n_time_by_label(labels, overlap_labels):
+    is_overlap_labels = labels.isin(
+        overlap_labels.index.get_level_values(labels.name))
+    overlap_labels1 = labels.loc[is_overlap_labels]
+    overlap_labels1 = (overlap_labels1
+                       .groupby(overlap_labels1)
+                       .agg(len)
+                       .rename(f'total_{overlap_labels1.name}'))
+
+    return overlap_labels1
+
+
+def compare_overlap(labels1, labels2, info1, info2):
+    labels1 = labels1.copy().rename('labels1')
+    labels2 = labels2.copy().rename('labels2')
+    is_overlap = (labels1 > 0) & (labels2 > 0)
+
+    overlap_labels = pd.concat(
+        (labels1.loc[is_overlap], labels2.loc[is_overlap]), axis=1)
+    overlap_labels = (overlap_labels
+                      .groupby(overlap_labels.columns.tolist())
+                      .agg(len)
+                      .sort_index()
+                      .rename('n_overlap')
+                      .to_frame())
+
+    overlap_labels1 = _get_n_time_by_label(labels1, overlap_labels)
+    overlap_labels2 = _get_n_time_by_label(labels2, overlap_labels)
+
+    name1 = 'overlap_percentage1'
+    name2 = 'overlap_percentage2'
+
+    percentage_overlap = {
+        name1: lambda df: 100 * df.n_overlap / df[overlap_labels1.name],
+        name2: lambda df: 100 * df.n_overlap / df[overlap_labels2.name]
+    }
+
+    overlap_labels = (overlap_labels
+                      .join(overlap_labels1)
+                      .join(overlap_labels2)
+                      .assign(**percentage_overlap))
+
+    start_time1 = (info1
+                   .loc[overlap_labels.index.get_level_values(0).values,
+                        ['start_time', 'end_time']])
+    start_time2 = (info2
+                   .loc[overlap_labels.index.get_level_values(1).values,
+                        ['start_time', 'end_time']])
+
+    time_difference = (start_time1.values - start_time2.values)
+
+    overlap_labels['start_time_difference'] = (
+        time_difference[:, 0] / np.timedelta64(1, 's'))
+    overlap_labels['end_time_difference'] = (
+        time_difference[:, 1] / np.timedelta64(1, 's'))
+
+    return overlap_labels
