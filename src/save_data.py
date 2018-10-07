cdf04076cc842279f4a8ec7464e025a991be5c36
Eric Denovellis <edeno@bu.edu>
2018-10-07 11:54:39 -0400

    Reset index for dataframes

--- src/save_data.py
+++ src/save_data.py
@@ -1,23 +1,26 @@
+from os.path import join
+
 from loren_frank_data_processing import save_xarray
 from src.parameters import PROCESSED_DATA_DIR
-from os.path import join
 
 
 def save_replay_data(name, epoch_key, replay_info, replay_densities,
                      is_replay):
     save_xarray(PROCESSED_DATA_DIR, epoch_key, replay_densities,
                 f'{name}/replay_densities')
-    save_xarray(PROCESSED_DATA_DIR, epoch_key, replay_info.to_xarray(),
-                f'{name}/replay_info')
-    save_xarray(PROCESSED_DATA_DIR, epoch_key, is_replay.to_xarray(),
-                f'{name}/is_replay')
+    save_xarray(PROCESSED_DATA_DIR, epoch_key,
+                replay_info.reset_index().to_xarray(), f'{name}/replay_info')
+    save_xarray(PROCESSED_DATA_DIR, epoch_key,
+                is_replay.reset_index().to_xarray(), f'{name}/is_replay')
 
 
 def save_ripple_data(epoch_key, data):
     save_xarray(PROCESSED_DATA_DIR, epoch_key,
-                data['ripple_labels'].to_xarray(), '/ripple_labels')
+                data['ripple_labels'].reset_index().to_xarray(),
+                '/ripple_labels')
     save_xarray(PROCESSED_DATA_DIR, epoch_key,
-                data['ripple_times'].to_xarray(), '/ripple_times')
+                data['ripple_times'].reset_index().to_xarray(),
+                '/ripple_times')
 
 
 def save_detector_parameters(epoch_key, replay_detector):
