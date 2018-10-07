2859653b0b6bbb220a2ac676a1882d87a52ea3d2
Eric Denovellis <edeno@bu.edu>
2018-10-07 11:54:48 -0400

    Add figure directory

--- src/parameters.py
+++ src/parameters.py
@@ -10,6 +10,7 @@ SAMPLING_FREQUENCY = 1500
 ROOT_DIR = join(abspath(dirname(__file__)), pardir)
 RAW_DATA_DIR = join(ROOT_DIR, 'Raw-Data')
 PROCESSED_DATA_DIR = join(ROOT_DIR, 'Processed-Data')
+FIGURE_DIR = join(ROOT_DIR, 'figures')
 
 ANIMALS = {
     'bon': Animal(directory=join(RAW_DATA_DIR, 'Bond'),
