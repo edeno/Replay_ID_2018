81b93fbb73b0d8b4653bdcb9fba374fbc8fb709c
Eric Denovellis <edeno@bu.edu>
2018-10-19 21:08:21 -0400

    Increase walltime

--- scripts/run_figures_on_cluster.sh
+++ scripts/run_figures_on_cluster.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 
 #$ -P braincom
-#$ -l h_rt=4:00:00
+#$ -l h_rt=10:00:00
 #$ -l mem_total=125G
 #$ -j y
 #$ -o create_figure.qlog
@@ -9,4 +9,4 @@
 
 python create_figures.py
 
-# echo python create_figures.py | qsub -l h_rt=4:00:00 -pe omp 4 -P braincom -l mem_total=125G -j y -o create_figure.log
+# echo python create_figures.py | qsub -l h_rt=10:00:00 -pe omp 4 -P braincom -l mem_total=125G -j y -o create_figure.log
