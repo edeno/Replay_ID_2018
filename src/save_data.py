205304ad0f6f1dad4d474d2dfa3a2a9e958aad89
Eric Denovellis <edeno@bu.edu>
2018-10-25 16:23:25 -0400

    Save decoder posterior summed over state, not replay detector posterior
    Replay detector posterior not as useful when it is LFPs, because we can't see the motion of the replay

--- src/save_data.py
+++ src/save_data.py
@@ -6,8 +6,10 @@ from src.parameters import PROCESSED_DATA_DIR
 
 def save_replay_data(name, epoch_key, replay_info, replay_densities,
                      is_replay):
-    replay_densities = replay_densities.drop(
-        ['detector_likelihood', 'decoder_posterior']).drop('state')
+    replay_densities = (replay_densities
+                        .drop(['detector_likelihood', 'detector_posterior'])
+                        .sum('state')
+                        .drop('state'))
     save_xarray(PROCESSED_DATA_DIR, epoch_key, replay_densities,
                 f'{name}/replay_densities')
     save_xarray(PROCESSED_DATA_DIR, epoch_key,
